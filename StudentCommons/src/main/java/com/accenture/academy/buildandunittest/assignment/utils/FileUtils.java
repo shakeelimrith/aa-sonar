cf2693d13fd626adcd56b216fff9cbd5a1b0e837
Imrith, Shakeel D <shakeel.d.imrith@accenture.com>
2018-06-26 18:19:04 +0400

    Delete

---
+++ StudentCommons/src/main/java/com/accenture/academy/buildandunittest/assignment/utils/FileUtils.java
@@ -0,0 +1,13 @@
+package com.accenture.academy.buildandunittest.assignment.utils;
+
+public final class FileUtils {
+
+	public static boolean isCsvFile(String fileName) {
+		return fileName != null && fileName.endsWith(".csv");
+	}
+
+	public static String[] splitFile(String line, String delimeter) {
+		return line.split(delimeter);
+	}
+
+}
