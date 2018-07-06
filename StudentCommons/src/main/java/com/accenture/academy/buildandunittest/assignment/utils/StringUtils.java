b6da7a534defb06d39d51f5979173d475dd0559a
Wasseem Suhabuth <wasseemsuhabuth@live.com>
2018-07-06 22:23:18 +0400

    cleaning

--- StudentCommons/src/main/java/com/accenture/academy/buildandunittest/assignment/utils/StringUtils.java
+++ StudentCommons/src/main/java/com/accenture/academy/buildandunittest/assignment/utils/StringUtils.java
@@ -1,6 +1,5 @@
 package com.accenture.academy.buildandunittest.assignment.utils;
 
-import java.math.BigDecimal;
 import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -9,19 +8,14 @@ import java.util.regex.PatternSyntaxException;
 
 public class StringUtils {
 
-	static final public String EMPTY = "";
+	public static final String EMPTY = "";
 
-	private static final String MINUS = "-";
+	//private static final String MINUS = "-";
 
-	static public boolean isEmpty(String value) {
-		if (value != null) {
-			if (value.isEmpty()) {
+	public static boolean isEmpty(String value) {
+		if (((value != null)&& (value.isEmpty()))||(value.length() == 0)){
+			
 				return true;
-			}
-		}
-
-		if (value.length() == 0) {
-			return true;
 		}
 
 		return false;
@@ -42,14 +36,14 @@ public class StringUtils {
 			}
 
 		} catch (NullPointerException e) {
-			System.out.println(e);
+			//System.out.println(e);
+			System.err.println(e);
 		}
 
 		return false;
 	}
 
-	public static String getStringOfRegulareExpressionPattern(String string, String pattern)
-			throws PatternSyntaxException, NullPointerException {
+	public static String getStringOfRegulareExpressionPattern(String string, String pattern) {
 		if (StringUtils.isEmpty(string) || StringUtils.isEmpty(pattern)) {
 			throw new IllegalArgumentException(" The pattern or the string to search is empty");
 		}
@@ -69,13 +63,14 @@ public class StringUtils {
 	public static String concatenate(String... value) {
 		String value1 = "";
 		for (String string : value) {
-			value1 = value1 + value;
+			//value1 = value1 + string;
+			value1.concat(string);
 		}
-		return value1.toString();
+		return value1;
 	}
 
 	public static String convertValueToNullifnull(String firstValue) {
-		boolean result = false;
+		//boolean result = false;
 		if (firstValue == null) {
 			firstValue = "null";
 		}
@@ -94,15 +89,15 @@ public class StringUtils {
 		return result;
 	}
 
-	private static String createFiller(int numberOfFiller, char typeOfFiller) {
-		final StringBuilder fillerString = new StringBuilder();
-		for (int i = 0; i < numberOfFiller; i++) {
-			fillerString.append(typeOfFiller);
-		}
-		return fillerString.toString();
-	}
+//	private static String createFiller(int numberOfFiller, char typeOfFiller) {
+//		final StringBuilder fillerString = new StringBuilder();
+//		for (int i = 0; i < numberOfFiller; i++) {
+//			fillerString.append(typeOfFiller);
+//		}
+//		return fillerString.toString();
+//	}
 
-	public static String removeTrailingSpacesOrConvertToNull(String input, boolean convertToNull, boolean isPositive) {
+	public static String removeTrailingSpacesOrConvertToNull(String input, boolean convertToNull) {
 		if (input != null) {
 			final String rtrim = input.replaceAll("\\s+$", "");
 
