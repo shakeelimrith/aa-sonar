00c64a38ef39c0f1b6c5218c609c692a5f1b3341
Lubrun <veeresh.lubrun@accenture.com>
2018-07-11 18:04:09 +0400

    Third commit

--- StudentCommons/src/main/java/com/accenture/academy/buildandunittest/assignment/enumerations/GradeEnum.java
+++ StudentCommons/src/main/java/com/accenture/academy/buildandunittest/assignment/enumerations/GradeEnum.java
@@ -52,12 +52,12 @@ public enum GradeEnum {
 		return maxMarks;
 	}
 
-	private void setMinMarks(int minMarks) {
+	/*private void setMinMarks(int minMarks) {
 		this.minMarks = minMarks;
 	}
 
 	private void setMaxMarks(int maxMarks) {
 		this.maxMarks = maxMarks;
-	}
+	}*/
 
 }
