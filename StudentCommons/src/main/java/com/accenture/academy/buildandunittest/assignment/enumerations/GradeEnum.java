094977251e104a9bef7322658a2509488a8f5fc0
Huril <hemanta.devi.huril@accenture.com>
2018-07-11 08:54:38 +0400

    second

--- StudentCommons/src/main/java/com/accenture/academy/buildandunittest/assignment/enumerations/GradeEnum.java
+++ StudentCommons/src/main/java/com/accenture/academy/buildandunittest/assignment/enumerations/GradeEnum.java
@@ -39,7 +39,6 @@ public enum GradeEnum {
 	 * @return the minMarks
 	 */
 	public final int getMinMarks() {
-		int minMarks = 0;
 		return minMarks;
 	}
 
@@ -52,12 +51,5 @@ public enum GradeEnum {
 		return maxMarks;
 	}
 
-	public void setMinMarks(int minMarks) {
-		this.minMarks = minMarks;
-	}
-
-	public void setMaxMarks(int maxMarks) {
-		this.maxMarks = maxMarks;
-	}
 
 }
