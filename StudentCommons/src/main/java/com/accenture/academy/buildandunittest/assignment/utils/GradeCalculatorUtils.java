2da2ff0aec1025cf154f5dd3b4743013080c3580
Pertaub <arjoonsing.pertaub@accenture.com>
2018-07-12 08:55:00 +0400

    2nd commit

--- StudentCommons/src/main/java/com/accenture/academy/buildandunittest/assignment/utils/GradeCalculatorUtils.java
+++ StudentCommons/src/main/java/com/accenture/academy/buildandunittest/assignment/utils/GradeCalculatorUtils.java
@@ -39,15 +39,16 @@ public final class GradeCalculatorUtils {
 	 * @return The total marks
 	 */
 	public static Double computeMark(Double mark, Double percentage, boolean giveAdditionalMarks) {
-		giveAdditionalMarks = false;
+		boolean addMarks = false;
 		if (mark == null || percentage == null) {
 			return Double.valueOf(0.0);
 		}
 
-		if (giveAdditionalMarks) {
+		/*if (giveAdditionalMarks) {
 			return FastMath.ceil(mark * percentage);
-		}
-		final BigDecimal total = new BigDecimal(mark * percentage);
+		}*/
+		
+		final BigDecimal total = BigDecimal.valueOf( mark*percentage);
 		
 		return total.setScale(1, RoundingMode.CEILING).doubleValue();
 	}
--- StudentWeb/src/main/java/com/accenture/academy/buildandunittest/assignment/studentview/StudentViewManagedBean.java
+++ StudentWeb/src/main/java/com/accenture/academy/buildandunittest/assignment/studentview/StudentViewManagedBean.java
@@ -47,8 +47,7 @@ public class StudentViewManagedBean {
 
 		if (studentFile.exists() && FileUtils.isCsvFile(realPath)) {
 			boolean isFirstLine = true;
-			try {
-				BufferedReader br = new BufferedReader(new FileReader(studentFile));
+			try(BufferedReader br = new BufferedReader(new FileReader(studentFile))) {
 				while ((line = br.readLine()) != null) {
 					// We skip the 1st line.
 					if (isFirstLine) {
@@ -58,10 +57,10 @@ public class StudentViewManagedBean {
 					final String[] oneLine = FileUtils.splitFile(line, ";");
 
 					list.add(computeStudentGrade(oneLine));
-
 				}
 			} catch (IOException e) {
 				// Do nothing
+			
 			}
 		}
 
