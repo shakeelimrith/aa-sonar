3118b213bbd9781b6a7f2a8d88233e92d8a0539d
Stephen Sibaly <stephen.laval.sibaly@accenture.com>
2018-07-12 09:47:08 +0400

    changed code in NoCacheFilter

--- StudentWeb/src/main/java/com/accenture/academy/buildandunittest/assignment/web/filter/NocacheFilter.java
+++ StudentWeb/src/main/java/com/accenture/academy/buildandunittest/assignment/web/filter/NocacheFilter.java
@@ -25,9 +25,9 @@ public class NocacheFilter implements Filter {
     }
     
     public void destroy() {
-    	throw new UnsupportedOperationException();
+//    do nothing
     }
     public void init(FilterConfig fConfig) throws ServletException {
-    	throw new UnsupportedOperationException();
+// do nothing
     }
 }
